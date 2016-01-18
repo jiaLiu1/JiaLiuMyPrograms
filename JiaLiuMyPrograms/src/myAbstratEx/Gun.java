31325eadffbe5833dc6530e3335d4dff09e49a18
Calvin <Calvin@Calvin-PC>
2016-01-18 13:50:00 -0500

    Abstract

---
+++ JiaLiuMyPrograms/src/myAbstratEx/Gun.java
@@ -0,0 +1,55 @@
+package myAbstratEx;
+
+public abstract class Gun {
+	private String nameOfGun;
+	private int ammoCapacity;
+	private int totalAmmo;
+	
+	public Gun(String nameOfGun, int ammoCapacity, int totalAmmo)
+	{
+		this.nameOfGun = nameOfGun;
+		this.ammoCapacity = ammoCapacity;
+		this.totalAmmo = totalAmmo;
+	}
+	
+//	public int computeDamage()
+//	{
+//		System.out.println("Inside Gun computeDamage");
+//		return 0;
+//	}
+	
+	public void damageCheck()
+	{
+		System.out.println("Checking damage of " + this.nameOfGun);
+	}
+	   
+	public String getNameOfGun() {
+		return nameOfGun;
+	}
+	
+//	public void setNameOfGun(String nameOfGun) {
+//		this.nameOfGun = nameOfGun;
+//	}
+//	
+//	public int getAmmoCapacity() {
+//		return ammoCapacity;
+//	}
+//	
+//	public void setAmmoCapacity(int ammoCapacity) {
+//		this.ammoCapacity = ammoCapacity;
+//	}
+//	
+//	public int getTotalAmmo() {
+//		return totalAmmo;
+//	}
+//	
+//	public void setTotalAmmo(int totalAmmo) {
+//		this.totalAmmo = totalAmmo;
+//	}
+	
+	public abstract int computeDamage();
+	
+	public String toString(){
+		return nameOfGun + " [Ammo:" + ammoCapacity + "/" + totalAmmo + "]";
+	}
+}
---
+++ JiaLiuMyPrograms/src/myAbstratEx/gunStats.java
@@ -0,0 +1,33 @@
+package myAbstratEx;
+
+public class gunStats extends Gun{
+	private int damage;
+	
+	public gunStats(String nameOfGun, int ammoCapacity, int totalAmmo, int damage) {
+		super(nameOfGun, ammoCapacity, totalAmmo);
+		this.damage = damage;
+		//setDamage(damage);
+	}
+
+	public void damageCheck()
+	{
+		System.out.println("The gun: " + getNameOfGun()
+				+ "\n" + "Damage: " + damage);
+	}
+	
+	public int getDamage()
+	{
+		return damage;
+	}
+	
+//	public void setDamage(int newDamage)
+//	{
+//		damage = newDamage;
+//	}
+	
+	public int computeDamage()
+	{
+		System.out.println("Setting damage of " + getNameOfGun());
+		return damage;
+	}
+}
