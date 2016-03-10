52ef2165df35801661094533b8ef3fdbe21ce3ca
Student8 <Student8@10.8.33.127>
2016-03-10 13:52:22 -0500

    ds

---
+++ JiaLiuMyPrograms/src/animal/Dog.java
@@ -0,0 +1,15 @@
+package animal;
+
+public class Dog implements Animal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public void wagTail(){
+		
+	}
+}
